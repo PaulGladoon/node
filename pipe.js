09bdf03f9814af0fe68def12bcada2ee7ad95c37
Your Name <administrator@Administrators-MacBook-Pro-3.local>
2018-11-09 11:18:43 +0200

    add pipe

---
+++ pipe.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const zlib = require('zlib')
+
+const input = fs.createReadStream('lorem.txt')
+const output = fs.createWriteStream('lorem.md.gz')
+const gzip = zlib.createGzip()
+
+input.pipe(gzip).pipe(output)
+
+class ReadStream {
+  pipe (stream) {
+    this.on('data', part => stream.write(part))
+    return stream
+  }
+}
+
+module.exports = {
+  ReadStream
+}
