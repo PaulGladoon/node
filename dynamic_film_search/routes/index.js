bb03229a8b4f7d2822742e452126953e479d2be6
Paul Gladoon <paul.gladoon@gmail.com>
2018-11-26 13:53:48 +0200

    add_film_search_app

---
+++ dynamic_film_search/routes/index.js
@@ -0,0 +1,11 @@
+const publicData = require('./public')
+const home = require('./home')
+const search = require('./search')
+const notFound = require('./notFound')
+
+module.exports = {
+  publicData,
+  home,
+  search,
+  notFound
+}
